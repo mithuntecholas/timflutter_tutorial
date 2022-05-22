af3ba36c2df18c1871e94591ba19fb84e46e951f
MITHUN <mithun.kv@techolas.com>
2022-05-22 13:17:48 +0530

    third sessio commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,28 +19,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String text = "";
+
+  void changeText(String te) {
+    setState(() {
+      this.text = te;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("HOME")), body: HomePageWidget());
+        appBar: AppBar(
+          title: Text("HOME"),
+        ),
+        body: Column(children: <Widget>[
+          HomePageWidget(this.changeText),
+          Text(this.text)
+        ]));
   }
 }
 
 class HomePageWidget extends StatefulWidget {
+  final Function(String) callback;
+
+  HomePageWidget(this.callback);
+
   @override
   _HomePageWidgetState createState() => _HomePageWidgetState();
 }
 
 class _HomePageWidgetState extends State<HomePageWidget> {
   final controller = TextEditingController();
-  String text="";
-  void changeText(tex)
-  {
-    setState(() {
-      this.text=tex;
-    });
 
+  void clickSendButton() {
+    widget.callback(this.controller.text);
+    this.controller.clear();
   }
 
   @override
@@ -50,10 +72,11 @@ class _HomePageWidgetState extends State<HomePageWidget> {
         TextField(
           controller: this.controller,
           decoration: InputDecoration(
-              prefixIcon: Icon(Icons.chat_rounded), labelText: "type here"),
-          onChanged: (te)=> this.changeText(te),
+              prefixIcon: Icon(Icons.chat_rounded),
+              labelText: "type here",
+              suffixIcon: IconButton(
+                  icon: Icon(Icons.send_sharp), onPressed: clickSendButton)),
         ),
-        Text(this.text)
       ],
     );
   }
